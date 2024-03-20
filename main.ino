a5b3d4f9611762be919d79e6f96130bad9f2858d
Viacheslav Zhuravlev <vzhurav@ncsu.edu>
2024-03-20 18:21:56 -0400

    add code for limit switches
    - flag for reaching Negative Limit was added
    - program sends a UDP message when Negative Limit is reached

--- main.ino
+++ main.ino
@@ -1,16 +1,13 @@
-// 9 Jan 2024  ver. 0.04 : Two sensors for X-axis work good
+// 9 Jan 2024 ver. 0.05 : flag for reaching NegLimit was added. Program sends a UDP message when NegLimit is reached.
 
 // 20 Dec 2023   ver. 0.03 : UDP-based, can move 2 motors for the same distance
 // This is an attempt to make a receiver of two coordinates (X and Z) through UDP
 
-// To control two motors, please send a UDP packet to 192.168.0.121, port 8888.
-// (in Linux one can use netcat: nc -u 192.168.0.121 8888)
-// Format of a message: X:number Z:number
-// number is an integet, can be negative
-// Also works: Z:number X:number
-
+// To control two motors, please, send a UDP packet to 192.168.0.121, port 8888.
+// in Linux can use netcat: "nc -u 192.168.0.121 8888"
+// Message format: X:number Z:number, number is an integet, can be negative
+// Opposite order also works: Z:number X:number
 // 0-th motor controls X-axis (horiz.), 1-st motor controls Z-axis (vertical)
-// Eventually program should be ready to work with up to 4 motors
 
 /*Links:
  * ** ClearCore Documentation: https://teknic-inc.github.io/ClearCore-library/
@@ -24,7 +21,6 @@
 // Change the MAC address and IP address below to match your ClearCore's
 // MAC address and IP.
 byte mac[] = {0xAA, 0xBB, 0xCC, 0x00, 0x00, 0x01};
-//IPAddress ip(192, 168, 1, 177);
 IPAddress ip(192, 168, 0, 121);
 
 // The local port to listen for connections on.
@@ -44,10 +40,9 @@ bool usingDhcp = false;
 
 
 // --------- Motor Control block ---------
-// Specifies which motor to move.
+// Specifies what motor to move.
 // Options are: ConnectorM0, ConnectorM1, ConnectorM2, or ConnectorM3.
 /////#define motor ConnectorM0
-
 #define motor0 ConnectorM0
 #define motor1 ConnectorM1
 
@@ -55,7 +50,7 @@ bool usingDhcp = false;
 #define baudRate 9600
 
 // This example has built-in functionality to automatically clear motor alerts, 
-//	including motor shutdowns. Any uncleared alert will cancel and disallow motion.
+//  including motor shutdowns. Any uncleared alert will cancel and disallow motion.
 // WARNING: enabling automatic alert handling will clear alerts immediately when 
 //	encountered and return a motor to a state in which motion is allowed. Before 
 //	enabling this functionality, be sure to understand this behavior and ensure 
@@ -66,8 +61,13 @@ bool usingDhcp = false;
 
 // Define the velocity and acceleration limits to be used for each move
 // speed of 10000 is too high for Z-axis motor (Z-axis motor takes up to 5000 pulses per sec) 
-int velocityLimit = 3000; //10000; // pulses per sec
+int velocityLimit = 3000; // pulses per sec, 5000 is max for vertical Z-axis
 int accelerationLimit = 100000; // pulses per sec^2
+
+int X_NegLimitFlag = 0;  // flag = 1 while NegLimit sensor is reached
+int X_PosLimitFlag = 0;
+int Z_NegLimitFlag = 0;
+int Z_PosLimitFlag = 0;
 
 // Declares user-defined helper functions.
 // The definition/implementations of these functions are at the bottom of the sketch.
@@ -84,11 +84,9 @@ void motor_1_HandleAlerts();
 
 void setup() {
     // Put your setup code here, it will run once:
-    
+ 
     // Set up serial communication at a baud rate of 9600 bps then wait up to
     // 5 seconds for a port to open.
-    // Serial communication is not required for this example to run, however the
-    // example will appear to do nothing without serial output.
     Serial.begin(9600);
     uint32_t timeout = 5000;
     uint32_t startTime = millis();
@@ -97,7 +95,6 @@ void setup() {
     }
     
     // --------- Ethernet block ---------
-
     // Get the Ethernet module up and running.
     if (usingDhcp) {
         int dhcpSuccess = Ethernet.begin(mac);
@@ -116,7 +113,6 @@ void setup() {
     else {
         Ethernet.begin(mac, ip);
     }
-
     // Make sure the physical link is up before continuing.
     while (Ethernet.linkStatus() == LinkOFF) {
         Serial.println("The Ethernet cable is unplugged...");
@@ -125,7 +121,6 @@ void setup() {
     // Begin listening on the local port for UDP datagrams
     Udp.begin(localPort);
     // --------- end of Ethernet block ---------
-
 
 
     // --------- Motor Control block ---------
@@ -160,10 +155,8 @@ void setup() {
     // Enables the motor; homing will begin automatically if enabled
     /////motor.EnableRequest(true);
     /////Serial.println("Motor Enabled");
-
     motor0.EnableRequest(true);
     Serial.println("Motor 0 Enabled");
-
     motor1.EnableRequest(true);
     Serial.println("Motor 1 Enabled");
 
@@ -172,10 +165,10 @@ void setup() {
     uint32_t lastStatusTime = millis();
     Serial.println("Waiting for HLFB...");
     while (motor0.HlfbState() != MotorDriver::HLFB_ASSERTED &&
-			motor1.HlfbState() != MotorDriver::HLFB_ASSERTED &&
-			!motor0.StatusReg().bit.AlertsPresent && 
-			!motor1.StatusReg().bit.AlertsPresent) {
-		// Periodically prints out why the application is waiting
+        motor1.HlfbState() != MotorDriver::HLFB_ASSERTED &&
+        !motor0.StatusReg().bit.AlertsPresent && 
+        !motor1.StatusReg().bit.AlertsPresent) {
+		    // Periodically prints out why the application is waiting
         if (millis() - lastStatusTime > 100) {
             Serial.println("Waiting for HLFB to assert on both motors");
             lastStatusTime = millis();
@@ -248,7 +241,6 @@ void setup() {
         // M-0's negative limit switch is now set to IO-0 and enabled.
         Serial.println("IO-0 was successfully set for Negative Sensor for ConnectorM0");	
     }
-
     if (ConnectorM0.LimitSwitchPos(CLEARCORE_PIN_IO1)) {
         // M-0's positive limit switch is now set to IO-1 and enabled.
         Serial.println("IO-1 was successfully set for Positive Sensor for ConnectorM0");
@@ -258,16 +250,62 @@ void setup() {
 
 
 
-// This loop will wait to receive a packet from a remote source, then reply back
-void loop() {
-    // Put your main code here, it will run repeatedly:
+void loop() {    // Put your main code here, it will run repeatedly:
+    /************************ Reading Motor Register ************************/
+    MotorDriver *motor = &ConnectorM0; // or ConnectorM0
+    volatile const MotorDriver::StatusRegMotor &statusReg = motor->StatusReg();
+    //  uint32_t ClearCore::MotorDriver::StatusRegMotor::InNegativeLimit
+    //  when we register hitting the InNegativeLimit, we send a message once
+    if ((statusReg.bit.InNegativeLimit == 1) && (X_NegLimitFlag == 0) ) {
+        Serial.print("InNegativeLimit:  ");
+        Serial.println(statusReg.bit.InNegativeLimit);
+        X_NegLimitFlag = 1;
+        // temp-4
+        Serial.print("X_NegLimitFlag:  "); Serial.println(X_NegLimitFlag);
+
+        Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
+        Udp.write("\nHit Negative Limit Sensor on axis X");
+        Udp.endPacket();        
+    }
+    if ((statusReg.bit.InNegativeLimit == 0) && (X_NegLimitFlag == 1) ) {
+        X_NegLimitFlag = 0;
+        // temp-4
+        Serial.print("X_NegLimitFlag: "); Serial.println(X_NegLimitFlag);
+    }
+
+    // This block tells us when a motor is moving ("Moving") or is not moving ("Ready")
+    /*if (statusReg.bit.ReadyState == MotorDriver::MOTOR_MOVING) {
+        Serial.println("Motor is Moving");
+    }*/
+    /*Serial.print("Ready state:   ");
+    switch (statusReg.bit.ReadyState) {
+        case MotorDriver::MOTOR_DISABLED:
+            Serial.println("Disabled");
+            break;
+        case MotorDriver::MOTOR_ENABLING:
+            Serial.println("Enabling");
+            break;
+        case MotorDriver::MOTOR_FAULTED:
+            Serial.println("Faulted");
+            break;
+        case MotorDriver::MOTOR_READY:
+            Serial.println("Ready");
+            break;
+        case MotorDriver::MOTOR_MOVING:
+            Serial.println("Moving");
+            break;
+        default:
+            // something has gone wrong if this is printed
+            Serial.println("??? (Undefined)");
+    }*/
+    /************************ Reading Motor Register ************************/
+
 
     // Look for a received packet.
     int packetSize = Udp.parsePacket();
-
     if (packetSize > 0) {
         Serial.print("Received packet of size ");
-        Serial.print(packetSize);
+        Serial.print(packetSize); 
         Serial.println(" bytes.");
         // printing IP address 
         Serial.print("Remote IP: ");
@@ -310,7 +348,7 @@ void loop() {
         
         int i = 0;
         while (i <= bytesRead) {
-		        Serial.println("Waiting for HLFB to assert on both motors");
+		        //Serial.println("Waiting for HLFB to assert on both motors");
             // example: Serial.println((String)"x:"+x+" y:"+y);
             Serial.println((String)"packetReceived[" + i + "]: " + packetReceived[i]);
             
@@ -339,7 +377,7 @@ void loop() {
                 //while (packetReceived[t + i + 2] != '\0') {
                 while ( (packetReceived[t + i + 2] != ' ') && (packetReceived[t + i + 2] != '\0') ) {
                     temp_str[t] = packetReceived[t + i + 2];
-                    Serial.print("\nvalue of t: "); Serial.print(t);
+                    //Serial.print("\nvalue of t: "); Serial.print(t);
                     t++;
                 }
                 temp_str[t] = '\0';
@@ -353,35 +391,28 @@ void loop() {
             i++;    
 	      }
         // for debug purposes
-        Serial.println((String)"Integer dist_X: " + dist_X);
-        Serial.println((String)"Integer dist_Z: " + dist_Z);
+        //Serial.println((String)"Integer dist_X: " + dist_X);
+        //Serial.println((String)"Integer dist_Z: " + dist_Z);
 
         // Sending back reply packet back to the sender.
         Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
         Udp.write("Received X:");
         //char dist_char[8];                      // these two line is just 
-        //Udp.write(itoa(dist, dist_char, 10) );  // int-to-char convertion
+        //Udp.write(itoa(dist, dist_char, 10) );  // int-to-char conversion
         char X_dist_char[8];                      // these two line is just 
         Udp.write(itoa(dist_X, X_dist_char, 10) );  // int-to-char convertion
         Udp.write(" Received Z:");
         char Z_dist_char[8];                      // these two line is just 
-        Udp.write(itoa(dist_Z, Z_dist_char, 10) );  // int-to-char convertion
-
+        Udp.write(itoa(dist_Z, Z_dist_char, 10) );  // int-to-char conversion
         Udp.endPacket();
         
-     
-        int dist_0 = dist_X;
-        int dist_1 = dist_Z;
-        motor_0_MoveDistance(dist_0);
-        //delay(10); // wait 5 sec for motor_0_MoveDistance(dist_0) to finish
-        motor_1_MoveDistance(dist_1 + dist_1); // starting 2nd motor when the first stopped
-        delay(10); // actually, this works without a delay
-        
+        motor_0_MoveDistance(dist_X);
+        motor_1_MoveDistance(dist_Z);   
     }
 
-    delay(10);
-}
-
+    delay(10);    
+}  // end of loop()
+ 
 
 
 // Motor Control functions
